6512913b6d150cd395ce755138dec82523522836
Avinash Garg <avinash.garg>
2014-12-03 14:45:32 +0530

    rendering images in list view from network call

--- CustomListView/app/src/main/java/Adapters/RatingsAdapter.java
+++ CustomListView/app/src/main/java/Adapters/RatingsAdapter.java
@@ -1,20 +1,50 @@
 package Adapters;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.VolleyError;
 import com.example.avinashgarg.customlistview.MainActivity;
+import com.example.avinashgarg.customlistview.R;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 
+
+
 import Resources.Ratings;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+
 
 
 public class RatingsAdapter extends BaseAdapter {
 
     private MainActivity mainActivity;
     private ArrayList<Ratings> ratings;
+    Bitmap bitmap;
+    ImageView image;
 
    public RatingsAdapter(MainActivity mainActivity,ArrayList<Ratings> ratings)
    {
@@ -39,8 +69,85 @@ public class RatingsAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup viewParent) {
-        return null;
+        Log.i("service","!!!!!!!!!!!!!!!!!!!!!!!!:::::::::::::::::::::::::::::::");
+        View view = LayoutInflater.from(mainActivity).
+                inflate(R.layout.list_row,
+                        null);
+        image=(ImageView) view.findViewById(R.id.image);
+
+
+
+
+
+        TextView title=(TextView) view.findViewById( R.id.title);
+        TextView rating=(TextView) view.findViewById( R.id.rating);
+        TextView genre=(TextView) view.findViewById( R.id.genre);
+        TextView releaseYear=(TextView) view.findViewById( R.id.releaseYear);
+
+        title.setText(ratings.get(position).title);
+        rating.setText(ratings.get(position).rating);
+        releaseYear.setText(ratings.get(position).releaseYear);
+
+        String genreString="";
+
+        for(String str:ratings.get(position).genre)
+        {
+            genreString=str+ ", ";
+        }
+
+        Log.i("service","in adaptr:::::::::::::::::::::::::::::::"+genreString);
+
+        genre.setText(genreString);
+
+
+        Log.i("service","in adaptr:::::::::::::::::::::::::::::::"+ratings.get(position).image);
+
+
+
+
+
+        String img_url= ratings.get(position).image;
+        new LoadImage().execute(img_url);
+        return view;
+
+
+
+
     }
+
+
+    class  LoadImage extends AsyncTask<String,String,Bitmap>
+    {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream)new URL(args[0]).getContent());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+        protected void onPostExecute(Bitmap result) {
+            if(result != null){
+                image.setImageBitmap(result);
+
+            }else{
+                Toast.makeText(mainActivity, "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+
+
+
+
+
 
 
 }
--- CustomListView/app/src/main/java/com/example/avinashgarg/customlistview/MainActivity.java
+++ CustomListView/app/src/main/java/com/example/avinashgarg/customlistview/MainActivity.java
@@ -3,19 +3,29 @@ package com.example.avinashgarg.customlistview;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 import Adapters.RatingsAdapter;
+import Resources.Ratings;
 
 
 public class MainActivity extends Activity {
-
     private String jsonUrl="http://api.androidhive.info/json/movies.json";
     private ListView moviesRatingList;
     private RatingsAdapter ratingsAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +35,6 @@ public class MainActivity extends Activity {
         downloadResult.execute(jsonUrl);
         moviesRatingList=(ListView)findViewById(R.id.lstRatingList);
     }
-
 
     public class DownloadResult extends AsyncTask<String,Void,String>{
 
@@ -62,17 +71,68 @@ public class MainActivity extends Activity {
         }
     }
 
-
     public String getJsonData(String url) throws Exception
     {
-        return "";
+        URL jsonUrl=new URL(url);
+        HttpURLConnection httpURLConnection=(HttpURLConnection) jsonUrl.openConnection();
+//        Log.i("service","status is"+httpURLConnection.getResponseCode());
+        if(httpURLConnection.getResponseCode()==200)
+        {
+            return fetchData(httpURLConnection);
+        }
+        else
+        {
+            throw  new Exception() ;
+        }
     }
 
     public void processData(String result) throws Exception
     {
+//        Log.i("service","result data is:::::::");
+        ArrayList<Ratings> ratings = new ArrayList<Ratings>();
+        JSONArray jsonArray = new JSONArray(result);
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject ratingObject = jsonArray.getJSONObject(i);
+            String title = ratingObject.getString("title");
+            String  rating= ratingObject.getString("rating");
+
+            JSONArray genreArry = ratingObject.getJSONArray("genre");
+            ArrayList<String> genre = new ArrayList<String>();
+            for (int j = 0; j < genreArry.length(); j++) {
+                genre.add((String) genreArry.get(j));
+            }
+            String image=ratingObject.getString("image");
+            String yearOfRelease=ratingObject.getString("releaseYear");
+            ratings.add(new Ratings(title,image,rating,yearOfRelease, genre));
+        }
+        ratingsAdapter =new RatingsAdapter(this,ratings);
+        moviesRatingList.setAdapter(ratingsAdapter);
+
 
     }
 
+    public String fetchData(HttpURLConnection connection) throws Exception
+    {
 
+        StringBuilder stringBuilder = new StringBuilder();
+        String tmpString = "";
+
+        InputStream inputStream = connection.getInputStream();
+        InputStreamReader streamReader = new InputStreamReader(inputStream);
+        BufferedReader buffer = new BufferedReader(streamReader);
+
+        while ((tmpString = buffer.readLine()) != null) {
+            stringBuilder.append(tmpString);
+            stringBuilder.append(System.getProperty("line.separator"));
+        }
+
+        inputStream.close();
+        streamReader.close();
+        buffer.close();
+        String result = stringBuilder.toString();
+        return result;
+
+    }
 
 }
